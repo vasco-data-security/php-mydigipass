83f10833d05f5b3f67be51d97530c6b069d7e66b
MatthewVanKuyk <kuykma1@vasco.com>
2015-04-22 13:34:03 +0200

    Changed to the new function
    BASE_URI is always www.mydigipass.com and a Boolean in the button is used

--- mydigipass-config-example.php
+++ mydigipass-config-example.php
@@ -7,14 +7,15 @@
 define('CLIENT_ID',     '<your-client-id>');
 define('CLIENT_SECRET', '<your-client-secret>');
 define('REDIRECT_URI',  'http://localhost/php-mydigipass/index.php');
+define('MDP_BASE_URI', 'https://www.mydigipass.com');
 
 // Put this variable on true, when you're going into production.
 $production = false;
 
 
 if($production) {
-  define('MDP_BASE_URI', 'https://www.mydigipass.com');
+  define('MDP_SANDBOX', 'false');
 }
 else {
-  define('MDP_BASE_URI', 'https://sandbox.mydigipass.com');
+  define('MDP_SANDBOX', 'true');
 }
